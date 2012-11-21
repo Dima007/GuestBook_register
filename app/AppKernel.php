14e730af747de8fdb9aee04c5df1bb93359f8fb9
Dima Rymar <dimarymar@mail.ru>
2012-11-21 17:06:00 +0200

    first part

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Dima\MainBundle\MainBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new \Dima\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Dima/UserBundle/UserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dima\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
